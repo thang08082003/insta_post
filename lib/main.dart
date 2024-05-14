3db7f516d6caf1bf01c3dd458c76bc3ec549ae18
thang08082003 <nvthang8803@gmail.com>
2024-05-14 23:50:37 +0700

    thêm appbar và bottom appbar

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,82 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Instagram Post'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          elevation: 1,
+          color: Colors.white,
         ),
-        body: const InstagramPost(),
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const HomePage(),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: Image.asset(
+                "assets/images/home.png",
+                width: 25,
+                height: 25,
+              ),
+            ),
+            const Spacer(),
+            IconButton(
+              onPressed: () {},
+              icon: Image.asset(
+                "assets/images/search.png",
+                width: 25,
+                height: 25,
+              ),
+            ),
+            const Spacer(),
+            IconButton(
+              onPressed: () {},
+              icon: Image.asset(
+                "assets/images/more.png",
+                width: 25,
+                height: 25,
+              ),
+            ),
+            const Spacer(),
+            IconButton(
+              onPressed: () {},
+              icon: Image.asset(
+                "assets/images/like.png",
+                width: 25,
+                height: 25,
+              ),
+            ),
+            const Spacer(),
+            IconButton(
+              onPressed: () {},
+              icon: Image.asset(
+                "assets/images/user.png",
+                width: 25,
+                height: 25,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/ui/instagram_post.dart
+++ lib/ui/instagram_post.dart
@@ -1,82 +1,50 @@
 import 'package:flutter/material.dart';
 
-class InstagramPost extends StatelessWidget {
-  const InstagramPost({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Header Row
-          buildHeaderRow(),
-
-          const SizedBox(height: 16.0),
-
-          // Image
-          Image.asset('assets/images/abc.jpg', fit: BoxFit.cover),
-
-          const SizedBox(height: 16.0),
-
-          // Caption
-          const Text(
-            'The game in Japan was amazing and I want to share some photos.',
-            style: TextStyle(fontSize: 14.0),
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: Image.asset(
+            "assets/images/instagram.png",
+            height: 25.0,
+            width: 25.0,
           ),
-
-          const SizedBox(height: 16.0),
-
-          // Action buttons
-          buildActionButtons(),
+        ),
+        title: Image.asset(
+          "assets/images/instagram_logo.png",
+          height: 50.0,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Image.asset(
+              "assets/images/igtv.png",
+              height: 25.0,
+              width: 25.0,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Image.asset(
+              "assets/images/send.png",
+              height: 25.0,
+              width: 25.0,
+            ),
+          ),
         ],
+        centerTitle: true,
       ),
-    );
-  }
-
-  Row buildHeaderRow() {
-    return const Row(
-      children: [
-        CircleAvatar(
-          backgroundImage: AssetImage('assets/images/download.jpg'),
-          radius: 24.0,
-        ),
-        SizedBox(width: 16.0),
-        Text(
-          'joshua_',
-          style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-        ),
-        Spacer(),
-        IconButton(
-          icon: Icon(Icons.more_horiz),
-          onPressed: null,
-        ),
-      ],
-    );
-  }
-
-  Row buildActionButtons() {
-    return const Row(
-      children: [
-        IconButton(
-          icon: Icon(Icons.favorite),
-          onPressed: null,
-        ),
-        IconButton(
-          icon: Icon(Icons.comment),
-          onPressed: null,
-        ),
-        IconButton(
-          icon: Icon(Icons.share),
-          onPressed: null,
-        ),
-        Spacer(),
-        IconButton(
-          icon: Icon(Icons.bookmark),
-          onPressed: null,
-        ),
-      ],
     );
   }
 }
